1131b58f08a756ab692a021319d82a0c788037de
stereocat <stereocat@gmail.com>
2013-06-30 01:09:13 +0900

    generage graph with port number and per-port connection

--- my-routing-switch/lib/view/graphviz.rb
+++ my-routing-switch/lib/view/graphviz.rb
@@ -13,12 +13,23 @@ module View
 
       switch = {}
       topology.each_switch do | dpid, ports |
-        switch[ dpid ] = g.add_nodes( dpid.to_hex, "shape" => "box" )
+        port_labels = ports.map do |each|
+          "<port#{each.number}>#{each.number.to_s}"
+        end
+        switch[ dpid ] = g.add_nodes(
+          dpid.to_hex,
+          "shape" => "record",
+          "label" => "{DPID:#{dpid.to_hex} | {#{port_labels.join('|')}}}"
+        )
       end
 
       topology.each_link do | each |
         if switch[ each.dpid1 ] and switch[ each.dpid2 ]
-          g.add_edges switch[ each.dpid1 ], switch[ each.dpid2 ]
+          g.add_edges(
+            { switch[each.dpid1] => "port#{each.port1}" },
+            { switch[each.dpid2] => "port#{each.port2}" },
+            :dir => :none
+          )
         end
       end
 
